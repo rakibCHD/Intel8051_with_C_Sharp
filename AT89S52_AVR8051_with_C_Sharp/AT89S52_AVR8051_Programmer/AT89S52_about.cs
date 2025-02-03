1b1009d8d59c0c7dd146f11627f47462a5383b85
MD RAKIB CHOWDHURY <135139732+rakibCHD@users.noreply.github.com>
2025-02-03 20:36:41 +0600

    Update AT89S52_about.cs

--- AT89S52_AVR8051_with_C_Sharp/AT89S52_AVR8051_Programmer/AT89S52_about.cs
+++ AT89S52_AVR8051_with_C_Sharp/AT89S52_AVR8051_Programmer/AT89S52_about.cs
@@ -8,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+ 
 namespace AT89S52_AVR8051_Programmer
 {
     public partial class AT89S52_about : Form
